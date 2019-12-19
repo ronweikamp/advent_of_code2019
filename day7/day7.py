387da470e0a4aaf07f7482666bd69c03b9d3f0b8
Ron Weikamp <ron.weikamp@nedap.com>
2019-12-19 12:09:44 +0100

    part 1

--- day7/day7.py
+++ day7/day7.py
@@ -1,5 +1,5 @@
 from day5 import run
-
+import itertools
 
 def get_init_state():
     for line in open('input', 'r'):
@@ -14,3 +14,19 @@ def run_phase_setting(code, phase_settings):
         _, input_output = run(0, code, inputs=[p, input_output])
 
     return input_output
+
+def find_largest_out():
+
+    code = get_init_state()
+
+    largest = 0
+
+    for setting in itertools.permutations([0, 1, 2, 3, 4]):
+        out = run_phase_setting(code, setting)
+
+        if out > largest:
+            largest = out
+
+    return largest
+
+print('largest setting {}'.format(find_largest_out()))
